268a94d3621567dfe21d87d94107dbe3bdb01170
gunther <gansa213@gmail.com>
2021-05-10 00:04:52 +0900

    ローカル起動時のホットデプロイ化

--- GathaoWeb.NetCore/Startup.cs
+++ GathaoWeb.NetCore/Startup.cs
@@ -23,6 +23,10 @@ namespace GathaoWeb.NetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMvc()
+                .AddRazorRuntimeCompilation();
+
             services.AddControllersWithViews();
         }
 
