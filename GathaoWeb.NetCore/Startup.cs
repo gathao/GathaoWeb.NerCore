64444c6e5e92c14859e3e01f9ea0bec19dba15f7
gunther <gansa213@gmail.com>
2021-05-10 00:02:10 +0900

    MVCのプロジェクトで生成しなおした

--- GathaoWeb.NetCore/Startup.cs
+++ GathaoWeb.NetCore/Startup.cs
@@ -23,9 +23,7 @@ namespace GathaoWeb.NetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddMvc()
-                .AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,11 +35,10 @@ namespace GathaoWeb.NetCore
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -51,7 +48,9 @@ namespace GathaoWeb.NetCore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
