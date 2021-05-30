b2fa76460395658ec821fe9977f41caf141d5393
gunther <gansa213@gmail.com>
2021-05-30 22:28:32 +0900

    Gameページのトップ作成

---
+++ GathaoWeb.NetCore/Controllers/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GathaoWeb.NetCore.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult YukariHomerun()
+        {
+            return View();
+        }
+
+        public IActionResult Suzumari()
+        {
+            return View();
+        }
+
+        public IActionResult VoiceRoid()
+        {
+            return View();
+        }
+    }
+}
